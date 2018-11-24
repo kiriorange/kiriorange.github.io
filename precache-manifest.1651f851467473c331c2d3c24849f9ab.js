44412d34da55175712195a209a3d1fbc8391d1b7
themetalbacon <feladorn@gmail.com>
2018-11-23 22:56:03 -0500

    Updates

---
+++ precache-manifest.1651f851467473c331c2d3c24849f9ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5571d5242120e713b47d",
+    "url": "/site/static/js/runtime~main.5571d524.js"
+  },
+  {
+    "revision": "b7f2a3f4078d61d17eee",
+    "url": "/site/static/js/main.b7f2a3f4.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/site/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "b7f2a3f4078d61d17eee",
+    "url": "/site/static/css/main.b54930c9.chunk.css"
+  },
+  {
+    "revision": "942759c3cbaf528b0034f6cafde1f20f",
+    "url": "/site/index.html"
+  }
+];
