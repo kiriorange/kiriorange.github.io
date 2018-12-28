ffc549bf7ad5de36c78ceb2ca0ddc9cda0abc085
themetalbacon <feladorn@gmail.com>
2018-12-28 18:37:16 -0500

    Updates

---
+++ precache-manifest.898b31aad0e8b4f02baddabf3c12badf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6df8f0c5b8fcc2e4815e",
+    "url": "/static/js/main.6df8f0c5.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "6df8f0c5b8fcc2e4815e",
+    "url": "/static/css/main.b54930c9.chunk.css"
+  },
+  {
+    "revision": "d347af38adf9698a5e91854707a26221",
+    "url": "/index.html"
+  }
+];
