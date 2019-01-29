fafdc166794266172fca2ba7d54bb6984b30a3ca
Unknown <feladorn@gmail.com>
2019-01-28 21:22:12 -0500

    Update resume and contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,9 +8,8 @@ class Contact extends Component {
         <div>
           <h3 className="page-title">Contact</h3>
           <div className="content-text contacts">
-            <a href="https://www.linkedin.com/in/maik-rantetasik-070abb165/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i>LinkedIn</a>
-            <a href="mailto:maikgibranr@gmail.com" target="_blank" rel="noopener noreferrer"><i class="far fa-envelope"></i>Email</a>
-            <a href="https://github.com/maikgr" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i>Github</a>
+            <a href="mailto:zeke.tan@vanderbilt.edu" target="_blank" rel="noopener noreferrer"><i class="far fa-envelope"></i>Email</a>
+            <a href="https://github.com/kiriorange" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i>Github</a>
           </div>
         </div>
     )
